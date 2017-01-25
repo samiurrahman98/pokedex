69ec3e89963ac2e4edf1b5d3a080849dd3837786
samiurrahman98 <samiur-rahman@live.com>
2017-01-24 21:14:24 -0500

    Started designing UI

---
+++ pokedexApplication/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  pokedexApplication
+//
+//  Created by Samiur Rahman on 2017-01-24.
+//  Copyright © 2017 Samiur Rahman. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon
+{
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String
+        {
+            return _name
+    }
+    
+    var pokedexID: Int
+        {
+            return _pokedexID
+    }
+    
+    init(name: String, pokedeID: Int)
+    {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
